b8350a2656ddf2dfee5efef16191b382be7dc125
Mix <honter_tribor@hotmail.com>
2023-12-25 01:16:10 +0800

    image storage

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
 use Illuminate\Http\Request;
 
 class AvatarController extends Controller
 {
-    public function update(Request $request)
+    public function update(UpdateAvatarRequest $request)
     {
-        $request->validate([
-            'avatar' => 'image',
-            // 'avatar' => 'required|image|max:2048',
-        ]);
-        dd($request->all());
-        // stroe avatar
-        return redirect(route('profile.edit'));
+        //        取得上傳檔案         儲存於指定資料夾
+        $path = $request->file('avatar')->store('public/avatars');
+        // 更新資料庫                        取得檔案放置目錄('底下的資料夾')."/檔案路徑"
+        auth()->user()->update(['avatar' => storage_path('app')."/$path"]);
+        // C:/Users/Mix/Documents/backend/example-app/storage/app/public/avatars/uoZoVB0MnROQ6pOOCYT33Bi469LJZF8PY0hZJxay.jpg
+        dd(auth()->user());
+        return redirect(route('profile.edit'))->with('message', 'Avatar updated successfully');
     }
 }
