5ecb84ec0823d2bb6ad4b77349b5464a5a628a2a
Mix <honter_tribor@hotmail.com>
2023-12-27 22:56:52 +0800

    form request data

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,19 +4,24 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request)
     {
-        //        取得上傳檔案         儲存於指定資料夾
+        //            取得上傳檔案             儲存於指定資料夾
         // $path = $request->file('avatar')->store('public/avatars');
-        $path = $request->file('avatar')->store('avatars', 'public');
-        // dd($path);
-        // 更新資料庫                        取得檔案放置目錄('底下的資料夾')."/檔案路徑"
+        // $path = $request->file('avatar')->store('avatars', 'public');
+        $path = Storage::disk('public')->put('avatar', $request->file('avatar'));
+
+        if ($oldAvatar = $request->user()->avatar) {
+            // 刪除 public 目錄下的檔案
+            Storage::disk('public')->delete($oldAvatar);
+        }
+
+        // 更新資料庫
         auth()->user()->update(['avatar' => $path]);
-        dd(auth()->user());
         return redirect(route('profile.edit'))->with('message', 'Avatar updated successfully');
     }
 }
