76a7f5b5c26343d6adcad4f0b1a5b0fbf742d351
Mix <honter_tribor@hotmail.com>
2024-01-08 00:18:31 +0800

    Help Ticket Project store ticket with file

--- app/Http/Requests/StoreTicketRequest.php
+++ app/Http/Requests/StoreTicketRequest.php
@@ -6,15 +6,15 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreTicketRequest extends FormRequest
 {
-    /**
+      /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
+      /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,7 +22,9 @@ class StoreTicketRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title'       => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'attachment'  => ['sometimes', 'file', 'mimes:png,jpg,jpeg,pdf'],
         ];
     }
 }
--- app/View/Components/Textarea.php
+++ app/View/Components/Textarea.php
@@ -11,7 +11,7 @@ class textarea extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct(public $default)
+    public function __construct()
     {
         //
     }
